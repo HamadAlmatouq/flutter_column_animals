305127bf73ceac5aecba5600ccc4e94652ced00d
Hamad Almatouq <hamad.almatouq@outlook.com>
2024-10-09 21:18:39 +0300

    Column animals -Adds title -Adds borders -Adds margins -Adds paddings -Fixed errors & problems

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,79 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    //My text style for this task
+    final TextStyle textStyle = TextStyle(
+      color: Colors.black,
+      fontSize: 15,
+      fontWeight: FontWeight.bold,
+    );
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: Text(
+            "My giraffe profile",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              // Shadow for the text
+              // shadows: [
+              //   Shadow(
+              //     color: Colors.black.withOpacity(0.5),
+              //     offset: Offset(2.0, 2.0),
+              //     blurRadius: 3.0,
+              //   ),
+              // ],
+            ),
+          ),
+          backgroundColor: Colors.yellow.shade400,
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.only(bottom: 20, top: 80),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle, //Make the container circular
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 2, // Border width
+                  ),
+                ),
+                child: ClipOval(
+                  child: Image.asset(
+                    'assets/images/MyAnimal.jpg',
+                    height: 150,
+                    width: 150,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(7),
+                margin: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.yellow.shade400,
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("Name: Melman", style: textStyle),
+                    Text("Gender: Male", style: textStyle),
+                    Text("Age: 35", style: textStyle),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
